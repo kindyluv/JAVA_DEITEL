f694524dfe6787a1500478b8326324974a4bcfd1
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-08-29 22:54:14 +0100

    Add files via upload

---
+++ ChapterNine/BillingInformation.java
@@ -0,0 +1,11 @@
+package ChapterNine;
+
+public class BillingInformation extends Customers{
+
+    private int receiverPhoneNumber;
+    private String receiverName;
+    private String DeliveryAddress;
+    private String CreditCardInformation;
+
+
+}
