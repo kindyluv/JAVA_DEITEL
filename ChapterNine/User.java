c3223d841fb28ae4bf7f3227e52d4aeef31c57c1
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-08-29 22:50:47 +0100

    Add files via upload

---
+++ ChapterNine/User.java
@@ -0,0 +1,10 @@
+package ChapterNine;
+
+public class User extends ElectronicStore{
+    private int age;
+    private String emailAddress;
+    private String HomeAddress;
+    private String useName;
+    private int password;
+    private int phone;
+}
