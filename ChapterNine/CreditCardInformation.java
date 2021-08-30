485359668dabf19ad5234a3c040f55027d7e8c89
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-08-30 13:25:49 +0100

    Add files via upload

--- ChapterNine/CreditCardInformation.java
+++ ChapterNine/CreditCardInformation.java
@@ -1,14 +1,12 @@
 package ChapterNine;
 
-public class CreditCardInformation extends BillingInformation{
+public class CreditCardInformation{
 
     private int cardCVV;
     private int CardExpirationMonth;
     private int cardExpirationYear;
     private int cardCardNumber;
     private String cardName;
-    private String CardType;
-
-    public enum CardType{MASTERCARD, VISA_CARD, VERVE, AMERICA_EXPRESS}
+    private CardType cardType;
 
 }
