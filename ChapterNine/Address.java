355641cecf5f025226f73e4f22ff88bb559dbe71
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-08-30 13:27:10 +0100

    Add files via upload

---
+++ ChapterNine/Address.java
@@ -0,0 +1,11 @@
+package ChapterNine;
+
+import java.util.ArrayList;
+
+public class Address {
+    private String cityName;
+    private String countryName;
+    private String houseNumber;
+    private String street;
+    private String state;
+}
