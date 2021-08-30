28495963ee36fd6db0df771fa7d4fc03003d549e
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-08-30 13:24:26 +0100

    Add files via upload

---
+++ ChapterNine/Product.java
@@ -0,0 +1,11 @@
+package ChapterNine;
+
+public class Product{
+    private int productId;
+    private String productName;
+    private int price;
+    private String productDescription;
+    private ProductCategory productCategory;
+
+
+}
