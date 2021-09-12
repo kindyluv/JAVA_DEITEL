6806fc91ee1c8194971b01d7b92828a03eb5930b
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-12 22:44:40 +0100

    Add files via upload

---
+++ ChapterSeven/PersonalSortArray.java
@@ -0,0 +1,30 @@
+package ChapterSeven;
+import java.util.Arrays;
+public class PersonalSortArray {
+
+    public static int[] sortArray(){
+        int highestNumber = 0;
+        int smallestNumber = 0;
+        int[] array = {3, 1, 6, 7, 2};
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for(int j = i+1; j < array.length; j++){
+                if (array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        smallestNumber = array.length;
+        highestNumber = array[4];
+        System.out.println(Arrays.toString(array));
+        System.out.printf("%d%n%d",smallestNumber, highestNumber);
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        sortArray();
+    }
+}
