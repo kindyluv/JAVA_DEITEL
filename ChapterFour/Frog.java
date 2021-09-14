aff6a48362ec2ef6bbcd44ddecd20212bf75fd34
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-14 19:44:06 +0100

    Add files via upload

---
+++ ChapterFour/Frog.java
@@ -0,0 +1,12 @@
+package ChapterTen;
+
+public class Frog extends Animal{
+
+    @Override
+    public void move() {
+        super.move();
+    }
+    public void method(CreditCard cc){
+        cc.someThings(3);
+    }
+}
