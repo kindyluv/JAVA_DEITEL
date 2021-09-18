bdb0f61376c3e5e0f426a99517ab557c6cfe1030
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-18 20:54:17 +0100

    Add files via upload

--- ChapterSeven/W3Resource.java
+++ ChapterSeven/W3Resource.java
@@ -19,16 +19,16 @@ public class W3Resource {
         System.out.println(Arrays.toString(array));
     }
 
-//    public static void sumArray() {
-//        int[] array = {5, 6, 3, 2, 8, 1, 0};
-//        int sum = 0;
-//        for (int i = 0; i < array.length; i++) {
-//            sum += array[i];
-//        }
-//        System.out.println(sum);
-//        ;
-//    }
-//
+    public static void sumArray() {
+        int[] array = {5, 6, 3, 2, 8, 1, 0};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum);
+        ;
+    }
+
 //    public static void averageSum() {
 //        int[] array = {5, 6, 3, 2, 8, 1, 0};
 //        int sum = 0;
