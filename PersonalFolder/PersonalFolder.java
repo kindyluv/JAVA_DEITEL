1abc27a0fdf5ac8e457018627dcfff5288760afd
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-20 21:39:38 +0100

    Add files via upload

--- PersonalFolder/PersonalFolder.java
+++ PersonalFolder/PersonalFolder.java
@@ -27,24 +27,24 @@ public class PersonalFolder {
         System.out.println("Choose\n" +
                 "1 to enter a string\n" +
                 "0 to quit");
-//        while (!quit){
-//            System.out.println("Choose an option: ");
-//            int choice = scanner.nextInt();
-//            switch (choice){
-//                case 0:
-//                    quit = true;
-//                    break;
-//                case 1:
-//                    System.out.print("Enter a string: ");
-//                    String stringInput = scanner.nextLine();
-//                    values.add(index, stringInput);
-//                    index++;
-//                    break;
-//            }
-//        }
-//        return values;
-//    }
-//
+        while (!quit){
+            System.out.println("Choose an option: ");
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
 //    public static  void saveObject(SaveAble objectToSave){
 //        for (int i = 0; i < objectToSave.write().size(); i++) {
 //            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
