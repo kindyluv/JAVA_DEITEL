06545b9742303f4a54fba526980017f8f2412a64
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-25 19:03:51 +0100

    Add files via upload

--- turtle/Pen.java
+++ turtle/Pen.java
@@ -3,11 +3,11 @@ import static ChapterSeven.turtleGraphics.PenPosition.*;
 
 public class Pen {
     private PenPosition penPosition = UP;
-//
-//    public PenPosition getPosition(){
-//        return penPosition;
-//    }
-//
+
+    public PenPosition getPosition(){
+        return penPosition;
+    }
+
 //    public void setPosition(PenPosition currentPosition){
 //        penPosition = currentPosition;
     }
