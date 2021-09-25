d1c064f5538de20573bc2847fdc92662a205ef17
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-25 19:00:25 +0100

    Add files via upload

---
+++ turtle/Position.java
@@ -0,0 +1,24 @@
+package ChapterSeven.turtleGraphics;
+
+public class Position {
+    private int rowPosition;
+    private int columnPosition;
+
+    public Position(int rowPosition, int columnPosition) {
+        this.rowPosition = rowPosition;
+        this.columnPosition = columnPosition;
+    }
+
+//
+//    @Override
+//    public String toString() {
+//        return "Position{}";
+//    }
+//
+//    @Override
+//    public boolean equals(Object o){
+////        Position comparedPosition = (Position);
+////        if ()
+//        return false;
+//    }
+}
