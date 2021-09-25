415dc3ade389c36a1b190b4bef852d5398cb2aa0
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-25 19:10:26 +0100

    Add files via upload

--- turtle/Turtle.java
+++ turtle/Turtle.java
@@ -43,7 +43,7 @@ public class Turtle {
         if (currentDirection == EAST) changeCurrentDirectionTo(NORTH);
     }
 
-//    public Position getCurrentPosition() {
-//        return new Position(0,0);
-//    }
+    public Position getCurrentPosition() {
+        return new Position(0,0);
+    }
 }
