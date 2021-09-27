a37ac73e4d2840200e2a975e75ab9ce5eff2f34c
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-09-27 17:26:38 +0100

    Add files via upload

--- turtle/TurtleTest.java
+++ turtle/TurtleTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static ChapterSeven.turtleGraphics.Direction. * ;
 import static ChapterSeven.turtleGraphics.PenPosition. * ;
-import static ChapterSeven.turtleGraphics.Position. *;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -128,7 +127,78 @@ class TurtleTest {
 
     @Test
     public void testThatTurtleCanMoveForwardWhileFacingEast(){
-        assertSame(new Position(0,0), ijapa.getCurrentPosition());
+        //given
+        assertSame(EAST, ijapa.getCurrentDirection());
+        assertEquals(new Position(0,0), ijapa.getCurrentPosition());
+        //when
+        ijapa.moveForward(5);
+        //assert
+        assertEquals(new Position(0,5), ijapa.getCurrentPosition());
     }
+//
+//    @Test
+//    public void testThatTurtleCanMoveForwardWhileFacingSouth(){
+//        //given
+//        assertSame(EAST, ijapa.getCurrentDirection());
+//        assertEquals(new Position(0,0), ijapa.getCurrentPosition());
+//        //when
+//        ijapa.turnRight();
+//        ijapa.moveForward(5);
+//        //assert
+//        assertEquals(new Position(5,0), ijapa.getCurrentPosition());
+//    }
+//
+//    @Test
+//    public void testThatTurtleCanMoveForwardWhileFacingWest(){
+//        //given
+////
+//        //when
+////        ijapa.turnLeft();
+//        ijapa.turnRight();
+//        ijapa.turnRight();
+////        assertSame(WEST, ijapa.getCurrentDirection());
+//        ijapa.moveForward(5);
+//        //assert
+//        assertEquals(new Position(0, 5), ijapa.getCurrentPosition());
+//    }
+//
+//    @Test
+//    public void testThatTurtleCanMoveForwardWhileFacingNorth(){
+//        ijapa.turnRight();
+//        ijapa.turnRight();
+//        ijapa.turnRight();
+//        ijapa.moveForward(5);
+//        //assert
+//        assertEquals(new Position(5, 0), ijapa.getCurrentPosition());
+//    }
+//
+//    @Test
+//    public void turtleCanWriteOnSketchPadWhileFAcingEastTest(){
+//        //given
+//        ijapa.penDown();
+//        assertSame(EAST, ijapa.getCurrentDirection());
+//        SketchPad sketchPad = new SketchPad(5,5);
+//        //when
+//        ijapa.writeOn(sketchPad, 3);
+//        //assert
+//        int[][] floor = sketchPad.getFloor();
+//        assertEquals(1, floor[0][0]);
+//        assertEquals(1, floor[0][1]);
+//        assertEquals(1, floor[0][2]);
+//        assertEquals(0, sketchPad.getFloor()[0][3]);
+//        assertEquals(new Position(0,2), ijapa.getCurrentPosition());
+//        sketchPad.displayFloor();
+//    }
+//
+//    @Test
+//    public void turtleCanWriteOnSketchPadWhileFAcingSouthTest() {
+//        //given
+//        ijapa.penUp();
+//        assertSame(SOUTH, ijapa.getCurrentDirection());
+//        SketchPad sketchPad = new SketchPad(5,5);
+//        //when
+//        ijapa.writeOn(sketchPad,);
+//
+//    }
 
 }
