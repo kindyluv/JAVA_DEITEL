3d3c39e287eace0a994b2479891b3ed9b8f1069c
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-02 23:36:15 +0100

    Add files via upload

--- ChapterEleven/creditCards/CreditCardTest.java
+++ ChapterEleven/creditCards/CreditCardTest.java
@@ -14,9 +14,26 @@ class CreditCardTest {
     }
 
     @Test
-    void testThatCreditCardCanOnlyBeBetween13And16DigitsLong(){
-        assertTrue(CreditCard.digit(1111111111111111L));
+    void testThatCreditCardCanNotBeBelow13DigitsLong(){
+        assertFalse(CreditCardValidation.digit(123456789012L));
     }
+
+//    @Test
+//    void testThatCreditCardCanNotBeAbove16DigitsLong(){
+//        assertFalse(CreditCardValidation.digit(111111111111111111L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardCanOnlyBeBetween13And16DigitsLong(){
+//        assertTrue(CreditCardValidation.digit(12345678901234L));
+//        assertTrue(CreditCardValidation.digit(123456789012345L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardCanOnlyBe13And16DigitsLong(){
+//        assertTrue(CreditCardValidation.digit(1234567890123L));
+//        assertTrue(CreditCardValidation.digit(1234567890123456L));
+//    }
 
     @Test
     void testThatCreditCardCanCalculateSumOfOddPlaces(){
@@ -28,18 +45,29 @@ class CreditCardTest {
         assertEquals(37,CreditCard.sumOfDoubleEvenPlace(4388576018402626L));
     }
 
-    @Test
-    void testThatCreditCardNumberCanBeValidated(){
-        assertTrue(CreditCard.isValid( 4388576018410707L));
-    }
-
-    @Test
-    void testThatCreditCardTypeVisaCardStartsWithFour4(long number){
-       assertTrue(CreditCard.checkVisaCard(4388576018410707L));
-    }
-
-    @Test
-    void testThatCreditCardTypeVisaCardStartsWithFive5(){
-        assertTrue(CreditCard.checkMasterCard(5388576018456788L));
-    }
+//    @Test
+//    void testThatCreditCardNumberCanBeValidated(){
+//        assertTrue(CreditCardValidation.isValid( 4388576018410707L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardTypeVisaCardStartsWithFour4(){
+//
+//       assertEquals(CreditCardType.VISA_CARD,CreditCard.checkCardType(4388576018410707L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardTypeVisaCardStartsWithFive5(){
+//        assertEquals(CreditCardType.MASTER_CARD,CreditCard.checkCardType(5388576018456788L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardTypeAmericanExpressCardStartsWith37(){
+//        assertEquals(CreditCardType.AMERICA_EXPRESS,CreditCard.checkCardType(3788576018456788L));
+//    }
+//
+//    @Test
+//    void testThatCreditCardTypeDiscoverCardStartsWith6(){
+//        assertEquals(CreditCardType.DISCOVER_CARD,CreditCard.checkCardType(6388576018456788L));
+//    }
 }
