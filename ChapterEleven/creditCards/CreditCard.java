7f8c09d8e81ec6a74e21bde9dd37ce8ce43706ed
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-02 21:19:36 +0100

    Add files via upload

--- ChapterEleven/creditCards/CreditCard.java
+++ ChapterEleven/creditCards/CreditCard.java
@@ -16,11 +16,11 @@ public class CreditCard {
         String masterCard = String.valueOf(number);
         return masterCard.charAt(0) == '5';
     }
-//
-//    public static boolean digit(long size) {
-//        String cardDigitLength = String.valueOf(size);
-//        return cardDigitLength.length() >= 13 && cardDigitLength.length() <= 16;
-//    }
+
+    public static boolean digit(long size) {
+        String cardDigitLength = String.valueOf(size);
+        return cardDigitLength.length() >= 13 && cardDigitLength.length() <= 16;
+    }
 //
 //    public static int sumOfOddPlaces(long number) {
 //        int sum = 0;
