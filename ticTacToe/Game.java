c4b686f216d6283943ae4062cc14f84bc48221bb
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-05 21:02:32 +0100

    Add files via upload

--- ticTacToe/Game.java
+++ ticTacToe/Game.java
@@ -17,8 +17,8 @@ public class Game {
           (!Objects.equals(playerBoard[0][0], empty) && playerBoard[0][0] == playerBoard[0][1] && playerBoard[0][0] == playerBoard[0][2])||
           (!Objects.equals(playerBoard[0][0], empty) && playerBoard[0][0] == playerBoard[1][0] && playerBoard[0][0] == playerBoard[2][0])||
 
-//          (!Objects.equals(playerBoard[1][0], empty) && playerBoard[1][0] == playerBoard[1][1] && playerBoard[1][0] == playerBoard[1][2])||
-//          (!Objects.equals(playerBoard[2][0], empty) && playerBoard[2][0] == playerBoard[2][1] && playerBoard[2][0] == playerBoard[2][2])||
+          (!Objects.equals(playerBoard[1][0], empty) && playerBoard[1][0] == playerBoard[1][1] && playerBoard[1][0] == playerBoard[1][2])||
+          (!Objects.equals(playerBoard[2][0], empty) && playerBoard[2][0] == playerBoard[2][1] && playerBoard[2][0] == playerBoard[2][2])||
 //
 //          (!Objects.equals(playerBoard[0][1], empty) && playerBoard[0][1] == playerBoard[1][1] && playerBoard[0][1] == playerBoard[2][1])||
 //          (!Objects.equals(playerBoard[0][2], empty) && playerBoard[0][2] == playerBoard[1][2] && playerBoard[0][2] == playerBoard[2][2])||
