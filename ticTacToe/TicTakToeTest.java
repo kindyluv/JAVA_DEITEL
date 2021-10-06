cbb2cd92935f83146d16244b79295e888b8db5f9
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-06 18:02:07 +0100

    Add files via upload

--- ticTacToe/TicTakToeTest.java
+++ ticTacToe/TicTakToeTest.java
@@ -1,76 +1,17 @@
 package ticTakToe;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static ticTakToe.Players.*;
-import static ticTakToe.TicTacToeType.*;
+import static ticTakToe.CellElement.*;
 
 
 class TicTakToeTest {
     TikTakToe ticTakToe = new TikTakToe();
-//    Board board = new Board(ticTakToe.cellElement);
-//    char[][] array = new char[3][3];
-////    Players players = ticTakToe.getPlayers();
-//
-//    @BeforeEach
-//    void setUp() {
-//        TikTakToe ticTakToe = new TikTakToe();
-//    }
-//
-////    @Test
-////    void testThatTicTacToeIsA3By3Array() {
-////        var array = ticTakToe.getArray();
-////        assertEquals(array, ticTakToe.getArray());
-////    }
-//
-//    @Test
-//    void testThatTicTacToeIsEmpty() {
-//        assertEquals(TicTacToeType.EMPTY, ticTakToe.getTicTacToe());
-//    }
-//
-//    @Test
-//    void testThatTicTacToePlayer1CanPlay() {
-//
-//        assertSame(PLAYER1, ticTakToe.getPlayers());
-//
-//    }
-//    @Test
-//    void testThatTicTacToePlayer2CanPlay() {
-//        ticTakToe.playersMood();
-//        assertSame(players.PLAYER2, ticTakToe.getPlayers());
-//    }
-//
-//    @Test
-//    void testThatTicTacToeCanDisplayTicTakToeBoard() {
-//        ticTakToe.displayBoard();
-//    }
-//
-//    @Test
-//    void testThatTicTacToePlayers1CanPlayIn1() {
-//        ticTakToe.play('X');
-//        assertEquals(1, players.getValue());
-//    }
-//
-//    @Test
-//    void testThatTicTacToePlayers2CanPlayIn2() {
-//        ticTakToe.play('O');
-//        assertEquals(0, players.getValue());
-//    }
-//    @Test
-//    void testThatTicTacToePlayerCanNotPlayOnSameCell() {
-//        ticTakToe.boardCell();
-////        assertEquals();
-//    }
-    @BeforeEach
-    void beforeEach(){
-        TikTakToe ticTakToe = new TikTakToe();
-
-    }
 
     @Test
-    void testThatTicTacToeHasABoard(){
-        assertNotNull(ticTakToe.getBoard());
+    void testThatTicTacToeHasAPlayingBoard(){
+        assertNotNull(ticTakToe.getPlayArray());
     }
 
     @Test
@@ -96,35 +37,34 @@ class TicTakToeTest {
     }
 
     @Test
-    void testThatPlayerOneCanPlay(){
-        ticTakToe.playerMove(PLAYER1,1);
-        assertEquals("X", Board.getBoard()[0][0]);
-        ticTakToe.playerMove(PLAYER1,4);
-        assertEquals("X", Board.getBoard()[1][0]);
-        ticTakToe.playerMove(PLAYER1,5);
-        assertEquals("X", Board.getBoard()[1][1]);
-        ticTakToe.playerMove(PLAYER1,7);
-        assertEquals("X", Board.getBoard()[2][0]);
+    void testThatPlayerOneCanMarkTheBoard(){
+        ticTakToe.currentPlayerMoves(PLAYER1,1);
+        assertEquals("X", Board.getPlayArray()[0][0]);
+        ticTakToe.currentPlayerMoves(PLAYER1,4);
+        assertEquals("X", Board.getPlayArray()[1][0]);
+        ticTakToe.currentPlayerMoves(PLAYER1,5);
+        assertEquals("X", Board.getPlayArray()[1][1]);
+        ticTakToe.currentPlayerMoves(PLAYER1,7);
+        assertEquals("X", Board.getPlayArray()[2][0]);
     }
 
     @Test
-    void testThatPlayerTwoCanPlay(){
-        ticTakToe.playerMove(PLAYER2,3);
-        assertEquals("O", Board.getBoard()[0][2]);
-        ticTakToe.playerMove(PLAYER2,2);
-        assertEquals("O", Board.getBoard()[0][1]);
-        ticTakToe.playerMove(PLAYER2,9);
-        assertEquals("O", Board.getBoard()[2][2]);
-        ticTakToe.playerMove(PLAYER2,6);
-        assertEquals("O", Board.getBoard()[1][2]);
-        ticTakToe.playerMove(PLAYER2,8);
-        assertEquals("O", Board.getBoard()[2][1]);
+    void testThatPlayerTwoCanMarkTheBoard(){
+        ticTakToe.currentPlayerMoves(PLAYER2,3);
+        assertEquals("O", Board.getPlayArray()[0][2]);
+        ticTakToe.currentPlayerMoves(PLAYER2,2);
+        assertEquals("O", Board.getPlayArray()[0][1]);
+        ticTakToe.currentPlayerMoves(PLAYER2,9);
+        assertEquals("O", Board.getPlayArray()[2][2]);
+        ticTakToe.currentPlayerMoves(PLAYER2,6);
+        assertEquals("O", Board.getPlayArray()[1][2]);
+        ticTakToe.currentPlayerMoves(PLAYER2,8);
+        assertEquals("O", Board.getPlayArray()[2][1]);
     }
 
     @Test
     void testThatTicTacToeCanPrintOnBoard(){
-        Board ticBoard = new Board();
-        ticBoard.printOnBoard();
+        Board.printOnBoard();
     }
 
     @Test
@@ -133,94 +73,76 @@ class TicTakToeTest {
     }
 
     @Test
-    void testThatPlayersCanOnlyMoveToAnEmptyCell(){
-        ticTakToe.play();
-        assertEquals(EMPTY, ticTakToe.getCellElement());
-    }
-
-    @Test
     void testThatTheGameCanBeWonByOneOfThePlayers(){
-        ticTakToe.play();
         assertEquals(EMPTY, ticTakToe.getCellElement());
-        ticTakToe.playerMove(PLAYER1,1);
-        ticTakToe.playerMove(PLAYER1,5);
-        ticTakToe.playerMove(PLAYER1,9);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+        ticTakToe.currentPlayerMoves(PLAYER1,1);
+        ticTakToe.currentPlayerMoves(PLAYER1,5);
+        ticTakToe.currentPlayerMoves(PLAYER1,9);
+        assertTrue(GameWinner.isWon(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInRowOne(){
-        ticTakToe.playerMove(PLAYER2,1);
-        ticTakToe.playerMove(PLAYER2,2);
-        ticTakToe.playerMove(PLAYER2,3);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInFirstRow(){
+        ticTakToe.currentPlayerMoves(PLAYER2,1);
+        ticTakToe.currentPlayerMoves(PLAYER2,2);
+        ticTakToe.currentPlayerMoves(PLAYER2,3);
+        assertTrue(GameWinner.firstRow(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInRowTwo(){
-        ticTakToe.playerMove(PLAYER1,4);
-        ticTakToe.playerMove(PLAYER1,5);
-        ticTakToe.playerMove(PLAYER1,6);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInSecondRow(){
+        ticTakToe.currentPlayerMoves(PLAYER1,4);
+        ticTakToe.currentPlayerMoves(PLAYER1,5);
+        ticTakToe.currentPlayerMoves(PLAYER1,6);
+        assertTrue(GameWinner.secondRow(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInRowThree(){
-        ticTakToe.playerMove(PLAYER2, 7);
-        ticTakToe.playerMove(PLAYER2,8);
-        ticTakToe.playerMove(PLAYER2,9);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInThirdRow(){
+        ticTakToe.currentPlayerMoves(PLAYER2, 7);
+        ticTakToe.currentPlayerMoves(PLAYER2,8);
+        ticTakToe.currentPlayerMoves(PLAYER2,9);
+        assertTrue(GameWinner.thirdRow(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInColumnOne(){
-        ticTakToe.playerMove(PLAYER1,1);
-        ticTakToe.playerMove(PLAYER1,4);
-        ticTakToe.playerMove(PLAYER1,7);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInFirstColumn(){
+        ticTakToe.currentPlayerMoves(PLAYER1,1);
+        ticTakToe.currentPlayerMoves(PLAYER1,4);
+        ticTakToe.currentPlayerMoves(PLAYER1,7);
+        assertTrue(GameWinner.firstColumn(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInColumnTwo(){
-        ticTakToe.playerMove(PLAYER2,2);
-        ticTakToe.playerMove(PLAYER2,5);
-        ticTakToe.playerMove(PLAYER2,8);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInSecondColumn(){
+        ticTakToe.currentPlayerMoves(PLAYER2,2);
+        ticTakToe.currentPlayerMoves(PLAYER2,5);
+        ticTakToe.currentPlayerMoves(PLAYER2,8);
+        assertTrue(GameWinner.secondColumn(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInColumnThree(){
-        ticTakToe.playerMove(PLAYER1,3);
-        ticTakToe.playerMove(PLAYER1,6);
-        ticTakToe.playerMove(PLAYER1,9);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInThreeColumn(){
+        ticTakToe.currentPlayerMoves(PLAYER1,3);
+        ticTakToe.currentPlayerMoves(PLAYER1,6);
+        ticTakToe.currentPlayerMoves(PLAYER1,9);
+        assertTrue(GameWinner.thirdColumn(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInDiagonalOne(){
-        ticTakToe.playerMove(PLAYER2,1);
-        ticTakToe.playerMove(PLAYER2,5);
-        ticTakToe.playerMove(PLAYER2,9);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInRightDiagonal(){
+        ticTakToe.currentPlayerMoves(PLAYER2,3);
+        ticTakToe.currentPlayerMoves(PLAYER2,5);
+        ticTakToe.currentPlayerMoves(PLAYER2,7);
+        assertTrue(GameWinner.rightDiagonal(Board.getPlayArray()));
     }
 
     @Test
-    void testThatGameCanBeWonInDiagonalTwo(){
-        ticTakToe.playerMove(PLAYER1,3);
-        ticTakToe.playerMove(PLAYER1,5);
-        ticTakToe.playerMove(PLAYER1,7);
-        assertTrue(Game.winningPlayer(Board.getBoard()));
+    void testThatGameCanBeWonInLeftDiagonal() {
+        ticTakToe.currentPlayerMoves(PLAYER1, 9);
+        ticTakToe.currentPlayerMoves(PLAYER1, 5);
+        ticTakToe.currentPlayerMoves(PLAYER1, 1);
+        assertTrue(GameWinner.leftDiagonal(Board.getPlayArray()));
     }
 
-//    @Test
-//    void testThatBoardCanNotBeOutOfBound(){
-//        assertTrue(Board.outOfBound());
-//    }
-
-//    @Test
-//    void testThatTicTacToeCanDetectFullBoard(){
-//        Board ticBoard = new Board();
-//        ticBoard.boardIsFull();
-//        assertEquals();
-//    }
 }
