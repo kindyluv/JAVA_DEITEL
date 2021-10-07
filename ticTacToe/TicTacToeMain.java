4629817f95aaf33935d99af1336c121d173d9952
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-07 21:35:54 +0100

    Add files via upload

--- ticTacToe/TicTacToeMain.java
+++ ticTacToe/TicTacToeMain.java
@@ -3,37 +3,62 @@ package ticTakToe;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static ticTakToe.CellElement.EMPTY;
-import static ticTakToe.Players.PLAYER1;
-import static ticTakToe.Players.PLAYER2;
+import static ticTakToe.CellElement.*;
+import static ticTakToe.Players.*;
 
 
 public class TicTacToeMain {
+    static Scanner scanner = new Scanner(System.in);
+    static TikTakToe ticTakToe = new TikTakToe();
+    static CellElement cellElement = EMPTY;
+    static Board board = new Board(cellElement);
+    static String[][] playBoard = board.getPlayArray();
+    static boolean stopGame = GameWinner.isWon(playBoard);
+    static Players player = PLAYER2;
 
-    static int playerMoves(String prompt){
-        Scanner scanner = new Scanner(System.in);
-        int input;
-        try {
-            System.out.println(prompt);
-            input = scanner.nextInt();
-        } catch (InputMismatchException | ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
-            return playerMoves(prompt);
-        }
-        return input;
+
+    public static void main(String[] args) {
+        playGame();
     }
-
-
-
-
-
-    public static void main(String[] args) throws Exception {
-        WinnerBoard winnerBoard = new WinnerBoard();
-        winnerBoard.winningPlayer();
-    }
-
-
-
-
+//
+//    public static void playGame() {
+//        String gamePlayer = "";
+//        while (!stopGame) {
+//            player = ticTakToe.getPlayers(player);
+//            if (player == PLAYER1) gamePlayer = "Player 1";
+//            else if (player == PLAYER2) gamePlayer = "Player 2";
+//            getInputFromPlayer(player);
+//            stopGame = GameWinner.isWon(playBoard);
+//            printOnBoard();
+//        }
+//        System.out.println(gamePlayer + " IS THE WINNER OF THE GAME!");
+//
+//    }
+//
+//    private static void getInputFromPlayer(Players player) {
+//        try {
+//            System.out.printf("%s: ", player.name());
+//            int input = scanner.nextInt();
+//            ticTakToe.currentPlayerMarks(player, input);
+//        } catch (InputMismatchException | ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+//            System.err.println(e.getMessage());
+//            getInputFromPlayer(player);
+//        }
+//    }
+//
+//    public static void printOnBoard() {
+//        System.out.println("_____________");
+//        for (int row = 0; row < playBoard.length; row++) {
+//            System.out.print("| ");
+//            for (int column = 0; column < playBoard[row].length; column++) {
+//                if (playBoard[row][column].equals(String.valueOf(EMPTY)))
+//                    System.out.print("_");
+//                else System.out.print(playBoard[row][column]);
+//                System.out.print(" | ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("_____________");
+//    }
 }
 
