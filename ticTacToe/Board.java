097b344f694124bad868e82ba54d8f4b577baa29
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-07 21:43:29 +0100

    Add files via upload

--- ticTacToe/Board.java
+++ ticTacToe/Board.java
@@ -8,21 +8,9 @@ public class Board {
     public static String[][] array = new String[row][column];
 
 
-    //    private static char[][] array = new char[row][column];
     public Board(CellElement cellElement) {
         for (String[] strings : array) {
             Arrays.fill(strings, String.valueOf(cellElement));
-        }
-    }
-
-    public static void printOnBoard() {
-        System.out.println("_________");
-        for (int row = 0; row < array.length; row++) {
-            for (int column = 0; column < array[row].length; column++) {
-                if (column == array[row].length - 1) System.out.println(array[row][column]);
-                else System.out.print(array[row][column] + " | ");
-            }
-            System.out.println("_________");
         }
     }
 
