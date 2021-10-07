599bf2ad26a9d63e9a521fd2df91a3cbf91f3f4d
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-07 21:31:27 +0100

    Add files via upload

--- ticTacToe/TicTakToe.java
+++ ticTacToe/TicTakToe.java
@@ -6,54 +6,56 @@ import static ticTakToe.CellElement.*;
 class TikTakToe {
     private CellElement cellElement = EMPTY;
     private Board board = new Board(cellElement);
-    private Players players = PLAYER2;
-    private String[][] playArray = Board.getPlayArray();
+    private String[][] playArray = board.getPlayArray();
 
     public String[][] getPlayArray() {
         return playArray;
     }
 
-    public CellElement getCellElement() {
-        return cellElement;
-    }
-
-    public Players getPlayers() {
-        return players;
-    }
-
-    public void currentPlayerMoves(Players player, int position) {
-        if (position > 9) throw new ArrayIndexOutOfBoundsException("You can only play between 1 & 9");
-        else {
-            if (player == PLAYER1) setPosition(position, X);
-            else if (player == PLAYER2) setPosition(position, O);
-        }
-    }
-
-    public void setPlayer(Players player) {
-        this.players = player;
-    }
-
-    public String[][] getArray() {
-        return Board.array;
-    }
-
-    public void turnUserInputToBoardPosition(int position, CellElement board) {
-        switch (position){
-            case 1 -> playArray[0][0] = String.valueOf(board);
-            case 2 -> playArray[0][1] = String.valueOf(board);
-            case 3 -> playArray[0][2] = String.valueOf(board);
-            case 4 -> playArray[1][0] = String.valueOf(board);
-            case 5 -> playArray[1][1] = String.valueOf(board);
-            case 6 -> playArray[1][2] = String.valueOf(board);
-            case 7 -> playArray[2][0] = String.valueOf(board);
-            case 8 -> playArray[2][1] = String.valueOf(board);
-            case 9 -> playArray[2][2] = String.valueOf(board);
-        }
-    }
-
-    public void setPosition(int position, CellElement board){
-        turnUserInputToBoardPosition(position, board);
-    }
+//    public CellElement getCellElement() {
+//        return cellElement;
+//    }
+//
+//    public Players getPlayers(Players player) {
+//        if (player == PLAYER1) {
+//            player = PLAYER2;
+//        } else {
+//            player = PLAYER1;
+//        }
+//
+//        return player;
+//    }
+//
+//    public void currentPlayerMarks(Players player, int position) {
+//        if (position > 0 && position < 10) {
+//            if (player == PLAYER1) setPosition(position, X);
+//            else if (player == PLAYER2) setPosition(position, O);
+//        }
+//        else throw new ArrayIndexOutOfBoundsException("You can only play between 1 & 9");
+//    }
+//
+//    public void turnUserInputToBoardPosition(int position, CellElement cellElement) {
+//        switch (position){
+//            case 1 -> validateAndAcceptPlayerPosition(0,0,cellElement);
+//            case 2 -> validateAndAcceptPlayerPosition(0,1,cellElement);
+//            case 3 -> validateAndAcceptPlayerPosition(0,2,cellElement);
+//            case 4 -> validateAndAcceptPlayerPosition(1,0,cellElement);
+//            case 5 -> validateAndAcceptPlayerPosition(1,1,cellElement);
+//            case 6 -> validateAndAcceptPlayerPosition(1,2,cellElement);
+//            case 7 -> validateAndAcceptPlayerPosition(2,0,cellElement);
+//            case 8 -> validateAndAcceptPlayerPosition(2,1,cellElement);
+//            case 9 -> validateAndAcceptPlayerPosition(2,2,cellElement);
+//        }
+//    }
+//
+//    private void validateAndAcceptPlayerPosition(int rowPosition, int columnPosition,CellElement board) {
+//        if (!playArray[rowPosition][columnPosition].equals(EMPTY.name())) throw new IllegalArgumentException("Position is already filled");
+//        else playArray[rowPosition][columnPosition] = String.valueOf(board);
+//    }
+//
+//    public void setPosition(int position, CellElement board){
+//        turnUserInputToBoardPosition(position, board);
+//    }
 }
 
 
