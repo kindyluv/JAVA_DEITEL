7bbd6d58a60cec9b79cebbc1c2a77f8b9db1db7e
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-07 21:33:49 +0100

    Add files via upload

--- ticTacToe/TicTakToe.java
+++ ticTacToe/TicTakToe.java
@@ -34,20 +34,20 @@ class TikTakToe {
         else throw new ArrayIndexOutOfBoundsException("You can only play between 1 & 9");
     }
 
-//    public void turnUserInputToBoardPosition(int position, CellElement cellElement) {
-//        switch (position){
-//            case 1 -> validateAndAcceptPlayerPosition(0,0,cellElement);
-//            case 2 -> validateAndAcceptPlayerPosition(0,1,cellElement);
-//            case 3 -> validateAndAcceptPlayerPosition(0,2,cellElement);
-//            case 4 -> validateAndAcceptPlayerPosition(1,0,cellElement);
-//            case 5 -> validateAndAcceptPlayerPosition(1,1,cellElement);
-//            case 6 -> validateAndAcceptPlayerPosition(1,2,cellElement);
-//            case 7 -> validateAndAcceptPlayerPosition(2,0,cellElement);
-//            case 8 -> validateAndAcceptPlayerPosition(2,1,cellElement);
-//            case 9 -> validateAndAcceptPlayerPosition(2,2,cellElement);
-//        }
-//    }
-//
+    public void turnUserInputToBoardPosition(int position, CellElement cellElement) {
+        switch (position){
+            case 1 -> validateAndAcceptPlayerPosition(0,0,cellElement);
+            case 2 -> validateAndAcceptPlayerPosition(0,1,cellElement);
+            case 3 -> validateAndAcceptPlayerPosition(0,2,cellElement);
+            case 4 -> validateAndAcceptPlayerPosition(1,0,cellElement);
+            case 5 -> validateAndAcceptPlayerPosition(1,1,cellElement);
+            case 6 -> validateAndAcceptPlayerPosition(1,2,cellElement);
+            case 7 -> validateAndAcceptPlayerPosition(2,0,cellElement);
+            case 8 -> validateAndAcceptPlayerPosition(2,1,cellElement);
+            case 9 -> validateAndAcceptPlayerPosition(2,2,cellElement);
+        }
+    }
+
 //    private void validateAndAcceptPlayerPosition(int rowPosition, int columnPosition,CellElement board) {
 //        if (!playArray[rowPosition][columnPosition].equals(EMPTY.name())) throw new IllegalArgumentException("Position is already filled");
 //        else playArray[rowPosition][columnPosition] = String.valueOf(board);
