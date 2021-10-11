12b5d8d2577f778c35b62585fc06318729928f60
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-11 13:26:16 +0100

    Add files via upload

--- ticTacToe/TicTacToeMain.java
+++ ticTacToe/TicTacToeMain.java
@@ -6,59 +6,48 @@ import java.util.Scanner;
 import static ticTakToe.CellElement.*;
 import static ticTakToe.Players.*;
 
-
 public class TicTacToeMain {
-    static Scanner scanner = new Scanner(System.in);
-    static TikTakToe ticTakToe = new TikTakToe();
-    static CellElement cellElement = EMPTY;
-    static Board board = new Board(cellElement);
-    static String[][] playBoard = board.getPlayArray();
-    static boolean stopGame = GameWinner.isWon(playBoard);
-    static Players player = PLAYER2;
+    private static TikTakToe ticTakToe = new TikTakToe();
+    private static CellElement cellElement = EMPTY;
+    private static Board board = new Board(cellElement);
+    private static String[][] playBoard = Board.getPlayArray();
+    private static int numberOfPlaysCounter = 0;
+    static boolean stopGame = GameWinner.hasWon(playBoard) || GameWinner.isDraw(playBoard, numberOfPlaysCounter);
 
-
-    public static void main(String[] args) {
+    public static void main(String... args) {
         playGame();
     }
 
-    public static void playGame() {
-        String gamePlayer = "";
-        while (!stopGame) {
-            player = ticTakToe.getPlayers(player);
-            if (player == PLAYER1) gamePlayer = "Player 1";
-            else if (player == PLAYER2) gamePlayer = "Player 2";
-            getInputFromPlayer(player);
-            stopGame = GameWinner.isWon(playBoard);
-            printOnBoard();
-        }
-        System.out.println(gamePlayer + " IS THE WINNER OF THE GAME!");
+//    public static void playGame() {
+//        String gamePlayer = "";
+//
+//        while (!stopGame) {
+//            Players player = ticTakToe.getPlayer();
+//            if (player == PLAYER1) gamePlayer = "Player 1";
+//            else if (player == PLAYER2) gamePlayer = "Player 2";
+//            validateUserInputAndMarkBoardOf(player);
+//            ++numberOfPlaysCounter;
+//            stopGame = GameWinner.hasWon(playBoard) || GameWinner.isDraw(playBoard, numberOfPlaysCounter);
+//            System.out.println(ticTakToe.printOnBoard());
+//            ticTakToe.switchPlayers();
+//        }
+////        System.out.println(gamePlayer + " IS THE WINNER OF THE GAME!");
+//        if (GameWinner.hasWon(playBoard)) System.out.println(gamePlayer + " has won the game");
+//        else if (GameWinner.isDraw(playBoard, numberOfPlaysCounter)) System.out.println("The game is a tie");
+//    }
 
+//    private static void validateUserInputAndMarkBoardOf(Players player) {
+//        try {
+//            ticTakToe.currentPlayerMarks(player, collectInputFrom(player));
+//        } catch (InputMismatchException | ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+//            System.err.println(e.getMessage());
+//            validateUserInputAndMarkBoardOf(player);
+//        }
     }
 
-    private static void getInputFromPlayer(Players player) {
-        try {
-            System.out.printf("%s: ", player.name());
-            int input = scanner.nextInt();
-            ticTakToe.currentPlayerMarks(player, input);
-        } catch (InputMismatchException | ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            getInputFromPlayer(player);
-        }
-    }
-
-    public static void printOnBoard() {
-        System.out.println("_____________");
-        for (int row = 0; row < playBoard.length; row++) {
-            System.out.print("| ");
-            for (int column = 0; column < playBoard[row].length; column++) {
-                if (playBoard[row][column].equals(String.valueOf(EMPTY)))
-                    System.out.print("_");
-                else System.out.print(playBoard[row][column]);
-                System.out.print(" | ");
-            }
-            System.out.println();
-        }
-        System.out.println("_____________");
-    }
+//    private static int collectInputFrom(Players player) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("%s: ", player.name());
+//        return scanner.nextInt();
+//    }
 }
-
