49e2aed0af7ad3b4ccbf920ff134f7c99dea5c78
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-11 13:24:49 +0100

    Add files via upload

--- ticTacToe/GameWinner.java
+++ ticTacToe/GameWinner.java
@@ -1,7 +1,6 @@
 package ticTakToe;
 
 import java.util.Objects;
-
 import static ticTakToe.Board.getPlayArray;
 import static ticTakToe.CellElement.EMPTY;
 
@@ -9,58 +8,62 @@ public class GameWinner {
  static  String[][] playerBoard = getPlayArray();
    static String empty = String.valueOf(EMPTY);
 
-   public static boolean isWon(String[][] playerBoard) {
+   public static boolean hasWon(String[][] playerBoard) {
     return isWinningCondition(playerBoard);
    }
 
     private static boolean isWinningCondition(String[][] playerBoard) {
         return leftDiagonal(playerBoard) ||
-                rightDiagonal(playerBoard) ||
-                firstRow(playerBoard) ||
-                secondRow(playerBoard)||
-                thirdRow(playerBoard)||
-                firstColumn(playerBoard) ||
-                secondColumn(playerBoard)||
-                thirdColumn(playerBoard);
+                boardRightDiagonal(playerBoard) ||
+                boardFirstRow(playerBoard) ||
+                boardSecondRow(playerBoard)||
+                boardThirdRow(playerBoard)||
+                boardFirstColumn(playerBoard) ||
+                boardSecondColumn(playerBoard)||
+                boardThirdColumn(playerBoard);
     }
 
-    public static boolean thirdColumn(String[][] playerBoard) {
+//    public static boolean isDraw(String[][] playerBoard, int numberOfPlayerPlays) {
+//       return !hasWon(playerBoard) && numberOfPlayerPlays == 9;
+//    }
+
+    public static boolean boardThirdColumn(String[][] playerBoard) {
         return (!Objects.equals(playerBoard[0][2], empty) &&
                 playerBoard[0][2] == playerBoard[1][2] &&
                 playerBoard[0][2] == playerBoard[2][2]);
     }
 
-    public static boolean secondColumn(String[][] playerBoard) {
+    public static boolean boardSecondColumn(String[][] playerBoard) {
        return (!Objects.equals(playerBoard[0][1], empty) &&
                playerBoard[0][1] == playerBoard[1][1] &&
                playerBoard[0][1] == playerBoard[2][1]);
     }
 
-    public static boolean thirdRow(String[][] playerBoard) {
+    public static boolean boardThirdRow(String[][] playerBoard) {
         return (!Objects.equals(playerBoard[2][0], empty) &&
                 playerBoard[2][0] == playerBoard[2][1] &&
                 playerBoard[2][0] == playerBoard[2][2]);
     }
 
-    public static boolean secondRow(String[][] playerBoard) {
+    public static boolean boardSecondRow(String[][] playerBoard) {
         return (!Objects.equals(playerBoard[1][0], empty) &&
                 playerBoard[1][0] == playerBoard[1][1] &&
                 playerBoard[1][0] == playerBoard[1][2]);
     }
 
-    public static boolean rightDiagonal(String[][] playerBoard) {
+    public static boolean boardRightDiagonal(String[][] playerBoard) {
        return (!Objects.equals(playerBoard[0][2], empty) &&
                playerBoard[0][2] == playerBoard[1][1] &&
                playerBoard[0][2] == playerBoard[2][0]);
     }
 
-    public  static boolean firstColumn(String[][] playerBoard) {
+    public  static boolean boardFirstColumn(String[][] playerBoard) {
        return(!Objects.equals(playerBoard[0][0], empty) &&
                playerBoard[0][0] == playerBoard[1][0] &&
                playerBoard[0][0] == playerBoard[2][0]);
     }
 
-    public static boolean firstRow(String[][] playerBoard) {
+    public static boolean boardFirstRow(String[][] playerBoard) {
        return (!Objects.equals(playerBoard[0][0], empty) &&
                playerBoard[0][0] == playerBoard[0][1] &&
                playerBoard[0][0] == playerBoard[0][2]);
