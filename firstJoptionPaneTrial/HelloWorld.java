c27cb1a6eb0a8f8d7bc921cf8191bcf2d8902edd
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-10-14 13:31:04 +0100

    Add files via upload

--- firstJoptionPaneTrial/HelloWorld.java
+++ firstJoptionPaneTrial/HelloWorld.java
@@ -8,18 +8,18 @@ public class HelloWorld {
         JOptionPane.showMessageDialog(null,"Hello World!","Love",JOptionPane.INFORMATION_MESSAGE);
     }
 
-//    public void sayHi(){
-//        JOptionPane.showInputDialog(null,"Saying Hi To The World", "Love");
-//    }
-//
-//    public void sayGreetings(){
-//        JOptionPane.showConfirmDialog(null, "Welcome", "FullStack",JOptionPane.YES_NO_OPTION);
-//    }
-//
-//    public void enterAnInteger(){
-//        JOptionPane.showInputDialog(null, "Enter an integer: ", "Input");
-//    }
-//
+    public void sayHi(){
+        JOptionPane.showInputDialog(null,"Saying Hi To The World", "Love");
+    }
+
+    public void sayGreetings(){
+        JOptionPane.showConfirmDialog(null, "Welcome", "FullStack",JOptionPane.YES_NO_OPTION);
+    }
+
+    public void enterAnInteger(){
+        JOptionPane.showInputDialog(null, "Enter an integer: ", "Input");
+    }
+
 //    public void enterAnIntegerNumber(){
 //        String number = JOptionPane.showInputDialog(null, "Enter an integer");
 //        int numb = Integer.parseInt(number);
