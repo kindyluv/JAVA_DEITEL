2b9608b76827e5c4481f5c3e5091fef5400510fd
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-17 19:09:15 +0100

    Add files via upload

---
+++ phoenieSchool/src/com/pheonix/service/PayStack.java
@@ -0,0 +1,13 @@
+package com.pheonix.service;
+
+import com.pheonix.models.Student;
+
+import java.math.BigDecimal;
+
+public class PayStack implements PaymentService{
+
+    @Override
+    public void processPayment(Student student, BigDecimal amount) {
+        //TODO
+    }
+}
