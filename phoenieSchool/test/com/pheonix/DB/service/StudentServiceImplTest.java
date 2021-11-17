923ccaab732aad735327ee8a4535340998dd6279
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-17 18:57:26 +0100

    Add files via upload

---
+++ phoenieSchool/test/com/pheonix/DB/service/StudentServiceImplTest.java
@@ -0,0 +1,98 @@
+package com.pheonix.service;
+
+import com.pheonix.DB.PhoenixSchoolDB;
+import com.pheonix.enums.Gender;
+import com.pheonix.exceptions.PhoenixSchoolException;
+import com.pheonix.exceptions.PhoenixStudentException;
+import com.pheonix.models.Student;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//import static com.pheonix.DB.PhoenixSchoolDB.students;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceImplTest {
+    StudentService studentService;
+
+    @BeforeEach
+    void setUp() {
+        studentService = new StudentServiceImpl();
+//        PhoenixSchoolDB.getTotalNumberOfStudent();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registerStudent() throws PhoenixSchoolException {
+        Student dami = new Student("Dami", "Omotosho", Gender.FEMALE);
+        assertEquals(2, PhoenixSchoolDB.getTotalNumberOfStudent());
+
+            dami = studentService.registerStudent(dami);
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+        assertEquals("PHX3", dami.getId());
+    }
+
+    @Test
+    void registerNullStudent(){
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+        assertThrows(PhoenixStudentException.class, () -> studentService.registerStudent(null));
+    }
+
+    @Test
+    void findStudent(){
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+        Student dami = null;
+        try{
+            dami = studentService.findStudent("PHX1");
+        }catch(PhoenixStudentException e){
+            e.printStackTrace();
+        }
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+        assertEquals("PHX1", dami.getId());
+    }
+
+    @Test
+    void FindStudentWithNull(){
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+//        assertThrows(PhoenixStudentException.class, () -> studentService.findStudent(null));
+    }
+
+    @Test
+    void findStudentWithInvalidStudentId(){
+        assertEquals(3, PhoenixSchoolDB.getTotalNumberOfStudent());
+        try{
+            Student unknown = studentService.findStudent("PHX200");
+            assertNull(unknown);
+        }catch(PhoenixStudentException e){
+            e.printStackTrace();
+        }
+        assertThrows(PhoenixStudentException.class, () -> studentService.registerStudent(null));
+
+    }
+
+//    @Test
+//    void updateStudent(){
+//        assertEquals(2, PhoenixSchoolDB.getTotalNumberOfStudent());
+//        try{
+//            Student dami = studentService.findStudent("PHX1");
+//            assertNotNull(dami);
+//            assertEquals("Dami", dami.getFirstName());
+//            assertEquals("Omotosho", dami.getLastName());
+//            assertNull(dami.getEmail());
+//
+//            dami.setEmail("dami@gmail.com");
+//            studentService.updateStudent(dami);
+//
+//            Student updatedDami = studentService.findStudent("PHX1");
+//            assertEquals("dami@gmail.com", updatedDami.getEmail());
+//        }catch(PhoenixStudentException e){
+//            e.printStackTrace();
+//        }
+//        assertThrows(PhoenixStudentException.class, () -> studentService.registerStudent(null));
+
+//    }
+}
