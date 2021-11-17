855d5731e596155d2bb8b9e98d9d75c99aafb6e0
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2021-11-17 19:00:39 +0100

    Add files via upload

---
+++ phoenieSchool/src/com/pheonix/exceptions/PhoenixStudentException.java
@@ -0,0 +1,15 @@
+package com.pheonix.exceptions;
+
+public class PhoenixStudentException extends PhoenixSchoolException{
+    public PhoenixStudentException(String message) {
+        super(message);
+    }
+
+    public PhoenixStudentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PhoenixStudentException(Throwable cause) {
+        super(cause);
+    }
+}
